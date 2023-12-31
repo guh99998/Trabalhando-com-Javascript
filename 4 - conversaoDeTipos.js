c1d5dca6c5a35a747cad5df1d2bb66d1161b0251
Gustavo Lopes <guhrlopes999@gmail.com>
2023-12-31 18:34:09 -0300

    Criado arquivo

---
+++ 4 - conversaoDeTipos.js
@@ -0,0 +1,8 @@
+console.log("Conversão de tipos")
+
+console.log("ano" + 2023);
+console.log("2" + "2");
+console.log("2" / "2"); //conversão efetuada pelo próprio interpretador
+console.log("Gustavo" / "2"); //Not a Number
+
+console.log(parseInt("2") + parseInt("2"));
