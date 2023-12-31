203cba0735d38c131cd0cd0adaa843701f3ce370
Gustavo Lopes <guhrlopes999@gmail.com>
2023-12-31 18:33:52 -0300

    Alterado arquivo

--- 1 - primeiroPrograma.js
+++ 1 - primeiroPrograma.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+console.log("Primeiro programa: Trabalhando com variáveis");
+
+const idade = 21;
+
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade + 4;
+
+console.log(idadeSomada)
