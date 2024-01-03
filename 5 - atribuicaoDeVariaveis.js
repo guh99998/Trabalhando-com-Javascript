029c48ea7c9269d55e85c4863c1e0760f9e25657
Gustavo Lopes <guhrlopes999@gmail.com>
2024-01-03 16:26:53 -0300

    Criado arquivo

---
+++ 5 - atribuicaoDeVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com atribuição de variáveis")
+
+const idade = 21;
+const primeiroNome = "Gustavo";
+const sobrenome = "Lopes";
+
+console.log("idade" + " " + idade);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+
+console.log(nomeCompleto);
