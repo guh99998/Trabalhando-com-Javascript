032f9dfcba6cfc1184a809eb7373d6426d545dee
Gustavo Lopes <guhrlopes999@gmail.com>
2024-01-12 10:14:36 -0300

    Alterado arquivo

--- Orientação a objetos/index.js
+++ Orientação a objetos/index.js
@@ -1,13 +1,23 @@
-class Cliente
-{
+class Cliente {
     nome;
     cpf;
 }
 
-class ContaCorrente
-{
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) 
+    {
+        if (this.saldo >= valor) 
+        {
+            this.saldo -= valor;
+            console.log("Novo saldo:", this.saldo);
+        } else 
+        {
+            console.log("Saldo insuficiente para sacar");
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -15,13 +25,19 @@ const cliente2 = new Cliente();
 
 cliente1.nome = "Gustavo";
 cliente1.cpf = 12345678911;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Cintia";
 cliente2.cpf = 150354897510;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+
+const contaCorrenteGustavo = new ContaCorrente();
+contaCorrenteGustavo.agencia = 1001;
+contaCorrenteGustavo.saldo = 0;
+console.log(contaCorrenteGustavo.saldo);
+contaCorrenteGustavo.saldo = 100;
+console.log(contaCorrenteGustavo.saldo);
+
+contaCorrenteGustavo.sacar(100);
 
 console.log(cliente1);
 console.log(cliente2);
